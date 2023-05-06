3695873696cd44eb33ab91cb6b0b5472c25370ef
Jess on the Hope <jesshon.py326@gmail.com>
2023-05-06 15:27:32 -0400

    finished file type analyzer
    using KMP to find patterns in file content to classify file type

--- File Type Analyzer (Kotlin)/File Type Analyzer (Kotlin)/task/src/analyzer/Main.kt
+++ File Type Analyzer (Kotlin)/File Type Analyzer (Kotlin)/task/src/analyzer/Main.kt
@@ -1,42 +1,69 @@
 package analyzer
 
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
+/*
     val pattern = args[0]
+    //println("pattern $pattern")
+
     val text = args[1]
+    //println("text $text")
+    val type = args[2]
+
     val array = buildPrefixFunction(pattern)
     println()
     for (each in array) {
         print("$each, ")
     }
 
-    KMP(pattern, text, array)
+    println(searchKMP(text, pattern, type))
 
-/*
+ */
+
     val algorithm = args[0]
     val filename = args[1]
     val pattern = args[2]
     val type = args[3]
 
     val file = File(filename)
-    val lines = file.readLines()
+    val text = file.readText()
 
     when (algorithm) {
-        "--navie" -> println(searchNaive(lines, pattern, type))
-        "--KMP" -> println()
+        "--naive" -> println(searchNaive(text, pattern, type))
+        "--KMP" -> println(searchKMP(text, pattern, type))
     }
-    //println()
-  */
 
 }
 
-private fun searchKMP(lines: List<String>, pattern: String,
-                      type: String) {
+private fun searchKMP(text: String, pattern: String,
+                      type: String) : String {
     // build prefix function
     // compare
+    var output = ""
+    val start = System.nanoTime()
+    val prefix = buildPrefixFunction(pattern)
+    val result = KMP(pattern, text, prefix)
+    val end = System.nanoTime()
+    if (result) {
+        //println("result is $result")
+        output += "$type\n"
+    } else {
+        output += "Unknown file type\n"
+    }
+    val time = formatTime(start, end)
+    output += "It took ${time} seconds"
 
+    return output
+}
 
+private fun formatTime(start: Long, end: Long) : String {
+    val elapsedTime = end - start
+    //val formattedTime = TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS)
+    //val formattedTime = TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS) / 1000.0
+    val formattedTime = elapsedTime.toDouble() / 1E9
+    return formattedTime.toString()
 }
 
 private fun buildPrefixFunction(pattern: String) : Array<Int> {
@@ -59,58 +86,83 @@ private fun buildPrefixFunction(pattern: String) : Array<Int> {
     return p
 }
 
-private fun KMP(pattern: String, text: String, prefixFunction: Array<Int>) {
+private fun KMP(pattern: String, text: String, prefixFunction: Array<Int>) : Boolean {
     var currentIndexText = 0
-    //var match = false
 
-    for (i in 0..text.length - 1) {
-        // compare pattern and text
+    while (currentIndexText < text.length) {
         var currentIndexPattern = 0
         var movingIndexText = currentIndexText
-        while (currentIndexPattern <= pattern.length) {
-            println("comparing ${text[currentIndexText]} ${pattern[currentIndexPattern]}")
-            println("moving index $movingIndexText current index pattern $currentIndexPattern")
+        //println("outter while")
+        //println("current index text $currentIndexText")
+        while (currentIndexPattern < pattern.length && movingIndexText < text.length) {
+            //println("inner while")
+            //println("comparing ${text[movingIndexText]} ${pattern[currentIndexPattern]}")
+            //println("currentIndex $currentIndexPattern movingIndex $movingIndexText")
+            //println("moving index $movingIndexText current index pattern $currentIndexPattern")
             if (text[movingIndexText] == pattern[currentIndexPattern]) {
-                // a match
-                println("match")
                 if (currentIndexPattern == pattern.length - 1) {
-                    println("complete match")
-                    break
+                    return true
                 }
-                // move 1 step
+                currentIndexPattern += 1
+                movingIndexText += 1
 
-                //match = true
             } else {
-                println("prefix function at $currentIndexPattern value ${prefixFunction[currentIndexPattern]}")
-                println("current index pattern $currentIndexPattern")
-                val step = currentIndexPattern - prefixFunction[currentIndexPattern]
-                currentIndexText += step
+                //println("prefix function at $currentIndexPattern value ${prefixFunction[currentIndexPattern]}")
+                //println("current index pattern $currentIndexPattern")
+                var step = 0
+                if (currentIndexPattern > 0) {
+                    step = currentIndexPattern - prefixFunction[currentIndexPattern - 1]
+                }
+                if (step > 0) {
+                    currentIndexText += step - 1
+                }
                 break
             }
-            println("move index one step")
-            currentIndexPattern++
-            movingIndexText++
-        }
-        // lookup prefix function for the pattern index
-        currentIndexText++
-        //prefixFunction[currentIndexPattern]
-    }
 
+        }
+        currentIndexText++
+    }
+    return false
+}
+private fun searchNaive(text: String,
+                        pattern: String,
+                          type: String) : String {
+
+    var output = ""
+    val start = System.nanoTime()
+
+    val result = naive(text, pattern)
+    val end = System.nanoTime()
+
+    if (result) {
+        output += "$type\n"
+    } else {
+        output += "Unknown file type\n"
+    }
+    val time = formatTime(start, end)
+    output += "It took ${time} seconds"
+
+    return output
 }
 
-
-private fun searchNaive(lines: List<String>,
-                          pattern: String,
-                          type: String) : String? {
-    for (line in lines) {
-        if (line.contains(pattern)) {
-            //return "${pattern.substring(1, 4)} document"
-            return type
-        } else {
-            return "Unknown file type"
+private fun naive(text: String,
+                  pattern: String,) : Boolean {
+    for (currentIndexText in 0..text.length - 1) {
+        var currentIndexPattern = 0
+        var movingIndex = currentIndexText
+        while (true) {
+            if (pattern[currentIndexPattern] == text[movingIndex]) {
+                movingIndex++
+                currentIndexPattern++
+                if (currentIndexPattern == pattern.length - 1) {
+                    return true
+                }
+            } else {
+                break
+            }
         }
     }
-    return null
+    return false
 }
 
 
